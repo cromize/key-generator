6f4c96e9c40f5d45f14c026230ca057442726e0b
cromize <mikezemanek@gmail.com>
2019-11-11 23:55:46 +0100

    add password based key generator

--- keygen.py
+++ keygen.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
+import hmac
 import base64
 import random
 import secrets
 import binascii
+from sys import exit
 
 # Tool for generating cryptographic keys
 
 # *** Key Generation ***
+
+# PBKDF2 (Password-Based Key Derivation Function 2)
+# key stretching
+def generate_key_password(passwd, salt, iterations, output_key_len):
+  from hashlib import sha256
+  rounds = int(output_key_len/256)
+  passwd = b""
+  for i in range(rounds or 1):
+    salt_i = salt + i.to_bytes(32, 'big')
+    for j in range(iterations):
+      passwd = hmac.new(passwd, salt_i, digestmod=sha256).digest()
+      salt_i = passwd
+  return passwd
 
 # Python's PRNG
 # size in bytes
@@ -29,15 +44,26 @@ def to_b64(x):
   return (base64.b64encode(x)).decode()
 
 if __name__ == "__main__":
-  print("** PRNG generated key")
+  print("** Password derived key")
+  passwd = b"this_is_password"
+  salt = generate_key_cprng(32)
+  print("password:", passwd.decode())
+  print("salt:", to_hex(salt))
+  passwd = generate_key_password(passwd, salt, 4096, 256//128)
+  print("key:", to_hex(passwd))
+  print("key length:", len(passwd), "bytes")
+
+  print("\n** PRNG generated key (NOT safe)")
   k = generate_key_prng(2*16)
   print("dec:", to_dec(k))
   print("hex:", to_hex(k))
   print("base64:", to_b64(k))
+  print("key length:", len(k), "bytes")
 
   print("\n** CPRNG generated key")
   k = generate_key_cprng(2*16)
   print("dec:", to_dec(k))
   print("hex:", to_hex(k))
   print("base64:", to_b64(k))
+  print("key length:", len(k), "bytes")
 
